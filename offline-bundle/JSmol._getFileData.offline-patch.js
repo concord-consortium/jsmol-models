a5942707a725b9a29ae52e1a9c106e9dab93e0a6
Piotr Janik <janikpiotrek@gmail.com>
2017-03-31 18:02:58 +0200

    Upgrade iframe-phone to v1.2.0
    [#139951055]

---
+++ offline-bundle/JSmol._getFileData.offline-patch.js
@@ -0,0 +1,35 @@
+Jmol._getFileData = function(fileName, fSuccess, doProcess) {
+  if (fileName.startsWith('file:/')) {
+    // Sometimes JSmol resolves paths to:
+    // file:/<full-path-of-the-jsmol-dir>/<path-of-the-file>
+    // We want to get rid of `file:/<full-path-of-the-jsmol-dir>/` part and leave just relative path, as this is ID
+    // of the script tag that contains its content in embeddable-offline.html. Example:
+    // fileName = "file:/Users/piotr/Concord/PascoInteractive/jsmol/models/dna/dna1.pdb"
+    // window.location.pathname = "/Users/piotr/Concord/PascoInteractive/jsmol/embeddable-offline.html"
+    // final fileName = "models/dna/dna1.pdb"
+    var path = window.location.pathname;
+    path = path.substr(0, path.lastIndexOf('/') + 1);
+    fileName = fileName.replace('file:' + path, '');
+  }
+  console.log('loading...', fileName);
+  var isBinary = Jmol._isBinaryUrl(fileName);
+  if (isBinary) {
+    console.warning('trying to load binary file:', fileName);
+  }
+  var data = null;
+  var localElement = document.getElementById(fileName);
+  if (localElement) {
+    console.log('loaded local!', fileName);
+    data = localElement.innerText;
+  } else {
+    console.error('missing data:', fileName);
+  }
+  if (!doProcess)
+    return data;
+  if (data == null) {
+    data = "";
+    isBinary = false;
+  }
+  isBinary && (isBinary = Jmol._canSyncBinary(true));
+  return (isBinary ? Jmol._strToBytes(data) : JU.SB.newS(data));
+};
