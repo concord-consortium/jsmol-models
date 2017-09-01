6922278a63f85dc13044c08e0f7e35b5ecd8a016
pjanik <janikpiotrek@gmail.com>
2017-09-01 16:23:06 +0200

    Update offline-bundle script to work with new JSmol version (v14)
    [#145363681]

--- offline-bundle/build.js
+++ offline-bundle/build.js
@@ -11,6 +11,11 @@ const outputDir = path.join(mainDir, 'jsmol-offline');
 function isJSFile(file) {
   // java/util/Random.js for some reason is breaking the whole page. It seems we can skip it and JSmol works anyway.
   if (file.path.indexOf('java/util/Random.js') !== -1) return;
+  // Also, some other files are including Math.seedrandom code which is breaking embeddable.html.
+  // Again, JSmol seems to work fine without those files (even though their names sound serious).
+  if (file.path.indexOf('core/corejmol.js') !== -1) return;
+  if (file.path.indexOf('core/corejsv.js') !== -1) return;
+  if (file.path.indexOf('core/corejmoljsv.js') !== -1) return;
   return file.path.match(/\.js$/);
 }
 
@@ -36,16 +41,13 @@ console.log('Generating offline bundle in ', outputDir);
 
 // Create or empty output dir.
 fs.emptyDirSync(outputDir);
-// Copy necessary JS libs.
-// JSmol injects some scripts using <script> tag, so we need to copy a few files (core.z.js and package.js)
-// and keep the original dir structure / paths.
-fs.copySync(path.join(mainDir, 'jsmol/j2s/core/core.z.js'), path.join(outputDir, 'jsmol/j2s/core/core.z.js'));
-fs.copySync(path.join(mainDir, 'jsmol/j2s/core/package.js'), path.join(outputDir, 'jsmol/j2s/core/package.js'));
 // Copy libs used by embeddable.html.
 fs.copySync(path.join(mainDir, 'jsmol/JSmol.min.js'), path.join(outputDir, 'jsmol/JSmol.min.js'));
 fs.copySync(path.join(mainDir, 'iframe-phone/dist/iframe-phone.js'), path.join(outputDir, 'iframe-phone/dist/iframe-phone.js'));
 fs.copySync(path.join(mainDir, 'shutterbug.js/dist/shutterbug.js'), path.join(outputDir, 'shutterbug.js/dist/shutterbug.js'));
 fs.copySync(path.join(mainDir, 'offline-bundle/JSmol._getFileData.offline-patch.js'), path.join(outputDir, 'JSmol._getFileData.offline-patch.js'));
+// Copy additional files used by JSmol.
+fs.copySync(path.join(mainDir, 'jsmol/j2s/img'), path.join(outputDir, 'jsmol/j2s/img'));
 
 // Start building new content that will be injected into embeddable.html.
 // 1. _getFileData overwrite that replaces all the Ajax calls with local HTML lookup.
